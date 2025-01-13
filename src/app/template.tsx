31b66fb45e01ce3816600499f0c663be7ad2003f
Lucasna28 <U2451146@rts-365.dk>
2025-01-13 14:09:50 +0100

    skeleton delete

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,28 +1,6 @@
 "use client";
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
-import Skeleton from "@/components/Skeleton";
+import { ReactNode } from "react";
 
-export default function Template({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    const timeout = setTimeout(() => {
-      setIsLoading(false);
-    }, 500);
-
-    return () => clearTimeout(timeout);
-  }, [pathname]);
-
-  return (
-    <>
-      {isLoading ? (
-        <Skeleton />
-      ) : (
-        <div className="transition-opacity duration-200">{children}</div>
-      )}
-    </>
-  );
+export default function Template({ children }: { children: ReactNode }) {
+  return <>{children}</>;
 }
